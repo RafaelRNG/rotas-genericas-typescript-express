db838851c2ba46e027354c6c8630a32699f100eb
RafaelRNG <rafael.gomila@hotmail.com>
2022-04-16 17:34:34 -0300

    Criando os metodos de create e update nas rotas genericas

--- backend/src/routes/User.router.ts
+++ backend/src/routes/User.router.ts
@@ -15,9 +15,9 @@ class UserRouter extends GenericRouter<UserInterface> {
    public applyRoutes(): void {
 
       this.router.get('', (request: Request, response: Response) => this.find(response, []))
-
       this.router.get('/:_id', (request: Request, response: Response) => this.findById(request, response, []))
-
+      this.router.post('', (request: Request, response: Response) => this.save(request, response))
+      this.router.put('/:_id', (request: Request, response: Response) => this.update(request, response))
       this.router.delete('/:_id', (request: Request, response: Response) => this.deleteById(request, response))
    }
 }
--- backend/src/utils/Generic.router.ts
+++ backend/src/utils/Generic.router.ts
@@ -5,6 +5,7 @@ export default abstract class GenericRouter<T extends Document> {
 
    public constructor(protected model: Model<T>) { }
 
+   //all documents
    public async find(response: Response, fields: string[]): Promise<Response> {
       const result = await this.model.find()
          .select([...fields])
@@ -12,24 +13,53 @@ export default abstract class GenericRouter<T extends Document> {
       return response.status(200).json(result)
    }
 
+   //find document by id
    public async findById(request: Request, response: Response, fields: string[]): Promise<Response> {
       try {
          const _id = request.params._id
          const result = await this.model.findById(_id)
             .select([...fields])
-         
+
          if (result === null || _id === null || !isValidObjectId(_id)) {
             return response.status(404).json({ message: 'Object not found!' })
          }
 
          return response.status(200).json(result)
-      
+
       } catch (e) {
          return response.status(404).json({ message: 'Object not found!' })
       }
    }
 
-   public async deleteById(request: Request, response: Response) {
+   //create a document
+   public async save(request: Request, response: Response): Promise<Response> {
+      try {
+         await this.model.create(request.body)
+         return response.status(201).json({ message: 'created!' })
+
+      } catch (e) {
+         return response.status(400).json({message: 'Cannot create! '})
+      }
+   }
+   
+   //update a document
+   public async update(request: Request, response: Response) {
+      try {
+         const _id = request.params._id
+         
+         if (_id === null || _id === undefined || !isValidObjectId(_id)) {
+            return response.status(404).json({ message: 'Object not found!' })
+         }
+         
+         await this.model.findByIdAndUpdate(_id, request.body)
+         return response.status(204).send()
+      } catch (e) {
+         return response.status(400).json({message: 'Cannot update! '})
+      }
+   }
+
+   //delete document by id
+   public async deleteById(request: Request, response: Response): Promise<Response> {
       try {
          await this.model.findByIdAndDelete(request.params._id)
          return response.status(204).send()
